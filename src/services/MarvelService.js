dcce4531064b7010db9155880e23853b82967bcd
Marsel <shaidullov-marsel@mail.ru>
2024-06-16 00:01:07 +0300

    first steps API

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
-
-
 class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=6d0ddc607d2d598b0b6af246749eba6a';
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -9,4 +10,29 @@ class MarvelService {
         }
         return await res.json();
     }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res .data.results.map(this._transformCharacter());
+
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 }
+
+export default MarvelService;
+
