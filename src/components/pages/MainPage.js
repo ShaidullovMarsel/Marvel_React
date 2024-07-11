a7bb7965e061e3062591e0f80470ff665c2b7c01
Marsel <shaidullov-marsel@mail.ru>
2024-07-11 23:39:18 +0300

    search form and helmet name

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary"; 
+import {Helmet} from "react-helmet";
 
 
 import decoration from '../../resources/img/vision.png';
@@ -17,6 +18,13 @@ const MainPage = () => {
 
     return (
         <>
+        <Helmet>
+            <meta
+                name="description"
+                content="Marvel information portal"
+            />
+            <title>Marvel information portal</title>
+        </Helmet>
         <ErrorBoundary>
             <RandomChar/>
         </ErrorBoundary>
